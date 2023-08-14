b5df79cff5599d38a33419085124d4ad5b250dbc
phoenix-ZY <hth_2003@sjtu.edu.cn>
2023-08-14 17:39:36 +0800

    update

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
 from utils import get_data, read_data
-import pdb
 read_data('data/附件2 慢性病及相关因素流调数据.xlsx')
 data = get_data()
-pdb.set_trace()
-data.statistics("food_diversity")
-
-pdb.set_trace()
+data.statistics("fresh_fruits")
