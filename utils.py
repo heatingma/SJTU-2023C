799967c969184f17387f654d9fa23cbaf346cd7f
heatingma <115260102+heatingma@users.noreply.github.com>
2023-08-15 13:24:03 +0800

    add the data in the statistics

--- utils.py
+++ utils.py
@@ -383,8 +383,8 @@ class Persons:
         self.stats_info = list()
         for attr in attrs:
             self._statistics(attr)
-            self.stats_ratio.append(getattr(self, "meet_"+attr).get_ratio())
-            self.stats_info.append(getattr(self, "meet_"+attr).get_info())
+            self.stats_ratio.append(getattr(self, attr).get_ratio())
+            self.stats_info.append(getattr(self, attr).get_info())
         self.stats_ratio = np.array(self.stats_ratio)
         self.stats_info = np.array(self.stats_info)
         self.stats_ratio = self.stats_ratio[np.argsort(-self.stats_ratio[:, 2].astype(float))]
@@ -394,16 +394,19 @@ class Persons:
         total = len(self.person_dict)
         effective = 0
         meet = 0
+        data_list = list()
         for person in self.person_dict.values():
             evaluate_info = getattr(person, "evaluate_info")
             evaluate_dict = getattr(evaluate_info, "evaluate_dict")
+            data_list.append(evaluate_dict[name])
             if evaluate_dict[name] is not None:
                 effective += 1
                 meet += int(evaluate_dict[name])
-        setattr(self, "meet_"+name, STATISTICS(name, total, effective, meet=meet))         
-        self.message += (", meet_" + name)
+        data_list = np.array(data_list)
+        setattr(self, name, STATISTICS(name, data_list, total, effective, meet=meet))         
+        self.message += (", " + name)
     
-    def draw(self):
+    def draw_ratio(self):
         plt.rcParams.update({'font.size': 12})
         plt.figure(figsize=(14, 10))
         data = pd.DataFrame(self.stats_ratio[:, 1:].astype(float), index=self.stats_ratio[:, 0], columns=['False', 'True'])
@@ -420,12 +423,13 @@ class Persons:
         bottom_plot.set_xticklabels(data.name, rotation=20, fontsize='small')
         plt.ylim(0, 1.1)
         plt.title("Evaluating Indicator")
-        plt.savefig("pics/stats.png")
+        plt.savefig("pics/stats_ratio.png")
 
-    def cal_average(self, attrs:list, name):
+    def get_line_data(self, attrs:list, name):
         total_val = 0
         total = len(self.person_dict)
         effective = 0
+        data_list = list()
         for person in self.person_dict.values():
             begin = True
             for attr in attrs:
@@ -434,13 +438,15 @@ class Persons:
                     begin = False
                 else:
                     var = getattr(var, attr)
+            data_list.append(var)
             if var is not None:
                 effective += 1
                 total_val += var
+        data_list = np.array(data_list)
         avarage = total_val / effective
-        setattr(self, "avg_"+name, STATISTICS(name, total, effective, avarage=avarage))
-        self.message += (", avg_" + name)
-        
+        setattr(self, name, STATISTICS(name, data_list, total, effective, avarage=avarage))
+        self.message += (", " + name)
+    
     def __repr__(self):
         return f"{self.__class__.__name__}({self.message})" 
 
@@ -462,8 +468,9 @@ class EVALUATE:
 
 
 class STATISTICS:
-    def __init__(self, name, total, effective, meet=None, avarage=None):
+    def __init__(self, name, data, total, effective, meet=None, avarage=None):
         self.name = name
+        self.data = data
         self.total = total
         self.effective = effective
         self.meet = meet
@@ -476,7 +483,7 @@ class STATISTICS:
         return [self.name, 1-self.meet/self.effective, self.meet/self.effective]
     
     def __repr__(self):
-        message = "name, total, effective"
+        message = "name, data, total, effective"
         if self.meet:
             message += ", meet"
         if self.avarage:
